342be9a611c31d5f405aeae5b3087e604c9b224a
Nikita Khabarov <khabarov.n.e@gmail.com>
2024-12-26 14:46:29 +0500

    36 lesson was added

---
+++ MLNetConsoleDemo/Session_36/InputModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.ML.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MLNetConsoleDemo.Session_36
+{
+        class InputModel
+        {
+                public string ImagePath { get; set; }
+                public string FruitName { get; set; }
+
+                [ColumnName("Features")]
+                public byte[] ImageBytes { get; set; }
+
+        }
+}
